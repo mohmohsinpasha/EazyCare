45189af906002f98a17791dfcec4db8acd947d91
MohammedShaizRahaman <mohammedsshaizr@gmail.com>
2025-01-24 22:53:24 +0530

    featured profile with form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css'
-
+import { useEffect, useState } from 'react';
 import Profile from './components/user/Profile'
 
 
 import Signin from './components/user/Signin';
+import UserInfo from './components/user/UserInfo';
+
 
 function App() {
+  const [edit, setEdit] = useState(false)
+  
+  const [data, setData] = useState([])
+  let handleEdit = ()=>{
+    setEdit(!edit)
+  }
+  let onSave = (formdata) =>{
+    setData(formdata);
+    console.log(data)
+  }
+  useEffect(()=>{
+    console.log("page state changed",data)
+  },[data])
   return (
     <div>
-      <Signin/>
+      <Profile data={data} handleEdit={handleEdit}/> 
+      {edit && <UserInfo onSave={onSave}  onClose={handleEdit}/>}
+
     </div>
   )
 
--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -1,37 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useContext, createContext } from 'react';
 
-export default function Profile() {
+export let data = createContext();
+
+export default function Profile({ handleEdit,data }) {
+
   return (
+    <div className="p-8">
 
-    <div className='p-8'>
-      <img className='w-36 rounded' src="https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg" alt="" />
+      <img
+        className="w-36 rounded"
+        src="https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
+        alt="Profile"
+      />
 
-      <h2 className='bg-gray-50 text-3xl font-medium max-w-60 mt-4 text-neutral-800'>Mohsin</h2>
-      <hr className='bg-zinc-400 h-[1px] border-none'/>
+      <h2 className="bg-gray-50 text-3xl font-medium max-w-60 mt-4 text-neutral-800">{data.name}</h2>
+      <hr className="bg-zinc-400 h-[1px] border-none" />
 
-      <h3 className='text-neutral-500 underline mt-3'>CONTACT INFORMATION</h3>
-      <table border={0} >
-        <tr >Email Id :
-          <td >xyz@gmail.com</td></tr>
-        <tr>Phone:
-          <td>+999999999</td></tr>
-        <tr>Address:
-          <td>###</td>
-        </tr>
+      <h3 className="text-neutral-500 underline mt-3">CONTACT INFORMATION</h3>
+      <table border={0}>
+        <tbody>
+          <tr>
+            <td>Email Id:</td>
+            <td>{data.email}</td>
+          </tr>
+          <tr>
+            <td>Phone:</td>
+            <td>{data.phone}</td>
+          </tr>
+          <tr>
+            <td>Address:</td>
+            <td>{data.address}</td>
+          </tr>
+        </tbody>
       </table>
-      <h3 className='text-neutral-500 underline mt-3'>BASIC INFORMATION</h3>
-      <table border={0} >
-        <tr >Gender :
-          <td >male</td></tr>
-        <tr>DOB
-          <td>25 dec 2001</td></tr>
+
+      <h3 className="text-neutral-500 underline mt-3">BASIC INFORMATION</h3>
+      <table border={0}>
+        <tbody>
+          <tr>
+            <td>Gender:</td>
+            <td>{data.gender}</td>
+          </tr>
+          <tr>
+            <td>DOB:</td>
+            <td>{data.dob}</td>
+          </tr>
+        </tbody>
       </table>
+
       <div className="mt-10">
-        <button className='border border-primary px-8 py-2 rounded-full hover:te'>Edit</button>
-        <button className='border border-primary px-8 py-2 rounded-full'> save informaition</button>
+        <button
+          className="border border-primary px-8 py-2 rounded-full hover:te"
+          onClick={() => {return handleEdit()}}
+        >
+          Edit
+        </button>
+        <button className="border border-primary px-8 py-2 rounded-full">
+          Save Information
+        </button>
       </div>
-
-
     </div>
-  )
+  );
 }
